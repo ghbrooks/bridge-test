d720f2d31b93f1448d889115976acca2230c64a8
George Brooks <george@brooks.cloud>
2021-12-22 15:56:57 +0100

    init and first page stylign

---
+++ components/Tutorial.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="outer">
+    <div class="inner">
+      <h1>Calculating your total costs</h1>
+      <ul class="cost-variable">
+        <btn class="cost-variable-item">
+          <img src="~assets/images/tick.svg" />
+          <p>Identify requirments</p>
+          <p class="cost-variable-item_estimate-text">
+            <span>Estimate</span>
+            </br><span> £100 </span>
+          </p>
+        </btn>
+        <li class="cost-variable-item">
+          <img src="~assets/images/tick.svg" />
+          <p>Identify requirments</p>
+          <p class="cost-variable-item_estimate-text">
+            <span>Estimate</span>
+            </br><span> £100 </span>
+          </p>
+        </li>
+        <li class="cost-variable-item">
+          <img src="~assets/images/tick.svg" />
+          <p>Identify requirments</p>
+          <p class="cost-variable-item_estimate-text">
+            <span>Estimate</span>
+            </br><span> £100 </span>
+          </p>
+        </li>
+        <li class="cost-variable-item">
+          <img src="~assets/images/tick.svg" />
+          <p>Identify requirments</p>
+          <p class="cost-variable-item_estimate-text">
+            <span>Estimate</span>
+            </br><span> £100 </span>
+          </p>
+        </li>
+        <li class="cost-variable-item">
+          <img src="~assets/images/tick.svg" />
+          <p>Identify requirments</p>
+          <p class="cost-variable-item_estimate-text">
+            <span>Estimate</span>
+            </br><span> £100 </span>
+          </p>
+        </li>
+        <li class="cost-variable-item">
+          <img src="~assets/images/tick.svg" />
+          <p>Identify requirments</p>
+          <p class="cost-variable-item_estimate-text">
+            <span>Estimate</span>
+            </br><span> £100 </span>
+          </p>
+        </li>
+        <li class="cost-variable-item">
+          <img src="~assets/images/tick.svg" />
+          <p>Identify requirments</p>
+          <p class="cost-variable-item_estimate-text">
+            <span>Estimate</span>
+            </br><span> £100 </span>
+          </p>
+        </li>
+        <li class="cost-variable-item">
+          <img src="~assets/images/tick.svg" />
+          <p>Identify requirments</p>
+          <p class="cost-variable-item_estimate-text">
+            <span>Estimate</span>
+            </br><span> £100 </span>
+          </p>
+        </li>
+        <li class="cost-variable-item">
+          <img src="~assets/images/tick.svg" />
+          <p>Identify requirments</p>
+          <p class="cost-variable-item_estimate-text">
+            <span>Estimate</span>
+            </br><span> £100 </span>
+          </p>
+        </li>
+        <li class="cost-variable-item">
+          <p>Total</p>
+          <p>£0.00</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+// export default {
+//   name: 'NuxtTutorial',
+// }
+</script>
